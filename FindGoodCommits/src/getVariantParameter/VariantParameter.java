e496e2e8989b18e3efa2ddac46ee9ad6aedbbeab
dollarmannen <dollarmannen@gmail.com>
2016-03-03 16:36:00 +0100

    Putte

--- FindGoodCommits/src/getVariantParameter/VariantParameter.java
+++ FindGoodCommits/src/getVariantParameter/VariantParameter.java
@@ -1,11 +1,57 @@
 package getVariantParameter;
 
-public class VariantParameter {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
 
-	public VariantParameter() {
+import utils.Utils;
+
+public class VariantParameter {
+	
+	private HashSet<String> mergeCommits;
+	private String REPO_PATH;
+	private String initialCommit;
+	private String parameter;
+
+	public VariantParameter(String repoPath, String initialCommit, String parameter) {
+		mergeCommits = new HashSet<String>();
+		REPO_PATH = repoPath;
+		this.initialCommit = initialCommit;
+		this.parameter = parameter;
+	}
+	
+	public HashMap<String, HashSet<String>> getCommitsWithParameterIntroduced() {
+		HashMap<String, HashSet<String>> commitToParameters = new HashMap<String, HashSet<String>>();
+		
+		
+		return commitToParameters;
+	}
+	
+	private HashSet<String> searchForParameters(String commit) {
+		HashSet<String> parameterNames = new HashSet<String>();
+		try {
+			BufferedReader br = Utils.readScriptOutput("bash scripts/getDiff " + REPO_PATH + " " + commit);
+			String line;
+			
+			while((line = br.readLine()) != null) {
+				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return null;
 		
 	}
 	
-	
+	private String extractParameter(String line) {
+		line = line.replace(" ", "");
+		return line.split("getAsBoolean(")[1].split(",")[0];
+	}
 	
 }
---
+++ FindGoodCommits/src/utils/Utils.java
@@ -0,0 +1,44 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Utils {
+
+	public static ArrayList<String> getAllMergeCommits(String repo) {
+		ArrayList<String> mergeCommits = new ArrayList<String>();
+		try {
+			Process p = Runtime.getRuntime().exec("bash scripts/getCommits " + repo);
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+			String commitSHA;
+			while((commitSHA = br.readLine()) != null) {
+				mergeCommits.add(commitSHA);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return mergeCommits;
+	}
+	
+	public static BufferedReader readScriptOutput(String command) throws IOException {
+		HashSet<String> parameters = new HashSet<String>();
+		Process p = Runtime.getRuntime().exec(command);
+		return new BufferedReader(new InputStreamReader(p.getInputStream()));
+	}
+	
+	public static void checkoutCommit(String repo, String commit) {
+		try {
+			readScriptOutput("bash scripts/checkoutCommit " + repo + " " + commit).readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
