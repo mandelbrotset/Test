edf40f7b95753199489b06cd759a009c91a0d3d8
Isak Eriksson <isak.eriksson@mail.com>
2016-03-16 12:28:11 +0100

    synchonized things

--- FindGoodCommits/src/findBooleans/FindGoodBooleans.java
+++ FindGoodCommits/src/findBooleans/FindGoodBooleans.java
@@ -4,23 +4,22 @@ import java.util.HashSet;
 
 public class FindGoodBooleans extends Thread {
 	private boolean inIfs;
-	
+
 	public FindGoodBooleans(boolean inIfs) {
 		this.inIfs = inIfs;
 	}
-	
+
 	@Override
 	public void run() {
 		findGoodBooleans();
 		super.run();
 	}
-	
+
 	private void findGoodBooleans() {
 		int total = Commander.commitToBooleanVariables.size();
 		int progress = 0;
 		for (String commit : Commander.commitToBooleanVariables.keySet()) {
-			Commander.print("findingGoodBooleans with " + inIfs + ": commit " + progress
-					+ " of " + total);
+			Commander.print("findingGoodBooleans with " + inIfs + ": commit " + progress + " of " + total);
 			progress++;
 			HashSet<String> variables = Commander.commitToBooleanVariables.get(commit);
 			HashSet<String> lines = Commander.commitToDiffPlus.get(commit);
@@ -29,24 +28,27 @@ public class FindGoodBooleans extends Thread {
 				for (String line : lines) {
 					if (inIfs) {
 						if (line.matches(".*(if).*[ ,(){}.&|=]+" + variable + "[ ,(){}.&|=]+.*")) {
-							if(line.contains("//"))
+							if (line.contains("//"))
 								line = line.split("//")[0];
-							
-							if(line.contains("/*"))
+
+							if (line.contains("/*"))
 								line = line.split("/*")[0];
-							
-							Commander.goodCommits.add(commit);
+							synchronized (Commander.goodCommits) {
+								Commander.goodCommits.add(commit);
+							}
 							goodVariables.add(variable + "|" + line);
 						}
 					} else {
 						String lowerLine = line.toLowerCase();
-						if(lowerLine.matches(".*(([ ,(){}.]+" + variable.toLowerCase() + "[ ,(){}.]+.*(setting|propert|config).*)|(setting|propert|config).*[ ,(){}.]+" + variable.toLowerCase() + "[ ,(){}.]+).*")){
-							if(line.contains("//"))
+						if (lowerLine.matches(".*(([ ,(){}.]+" + variable.toLowerCase()
+								+ "[ ,(){}.]+.*(setting|propert|config).*)|(setting|propert|config).*[ ,(){}.]+"
+								+ variable.toLowerCase() + "[ ,(){}.]+).*")) {
+							if (line.contains("//"))
 								line = line.split("//")[0];
-							
-							if(line.contains("/*"))
+
+							if (line.contains("/*"))
 								line = line.split("/*")[0];
-							
+
 							Commander.goodCommits.add(commit);
 							goodVariables.add(variable + "|" + line);
 						}
