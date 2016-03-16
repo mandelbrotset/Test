ebbc119f0056fb09a791c45825d8953491dd2a70
dollarmannen <dollarmannen@gmail.com>
2016-03-16 11:28:06 +0100

    Fixed japanese shit

--- FindGoodCommits/src/findBooleans/Commander.java
+++ FindGoodCommits/src/findBooleans/Commander.java
@@ -248,11 +248,7 @@ public class Commander {
 	}
 	
 	private void findGoodBooleans(boolean inIfs) {
-		int total = commitToBooleanVariables.size();
-		int progress = 0;
 		for(String commit : commitToBooleanVariables.keySet()) {
-			print("findingGoodBooleans with " + inIfs + ": commit " + progress + " of " + total);
-			progress++;
 			HashSet<String> variables = commitToBooleanVariables.get(commit);
 			HashSet<String> lines = commitToDiffPlus.get(commit);
 			HashSet<String> goodVariables = new HashSet<String>();
@@ -260,6 +256,12 @@ public class Commander {
 				for(String line : lines) {
 					if(inIfs) {
 						if(line.matches(".*(if).*[ ,(){}.&|=]+" + variable + "[ ,(){}.&|=]+.*")) {
+							if(line.contains("//"))
+								line = line.split("//")[0];
+							
+							if(line.contains("/*"))
+								line = line.split("/*")[0];
+							
 							goodCommits.add(commit);
 							goodVariables.add(variable + "|" + line);
 						}
@@ -267,6 +269,12 @@ public class Commander {
 					else {
 						String lowerLine = line.toLowerCase();
 						if(lowerLine.matches(".*(([ ,(){}.]+" + variable.toLowerCase() + "[ ,(){}.]+.*(setting|propert|config).*)|(setting|propert|config).*[ ,(){}.]+" + variable.toLowerCase() + "[ ,(){}.]+).*")){
+							if(line.contains("//"))
+								line = line.split("//")[0];
+							
+							if(line.contains("/*"))
+								line = line.split("/*")[0];
+							
 							goodCommits.add(commit);
 							goodVariables.add(variable + "|" + line);
 						}
