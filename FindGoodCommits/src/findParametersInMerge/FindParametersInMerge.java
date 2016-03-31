aedcd1c5dee1f9a52b260666f85cee38c3e1ec26
Isak Eriksson <isak.eriksson@mail.com>
2016-03-31 15:35:35 +0200

    made static

--- FindGoodCommits/src/findParametersInMerge/FindParametersInMerge.java
+++ FindGoodCommits/src/findParametersInMerge/FindParametersInMerge.java
@@ -7,43 +7,24 @@ import java.util.ArrayList;
 import utils.Utils;
 
 public class FindParametersInMerge {
-	private String c1 = "/home/isak/test/diff/c1";
-	private String c2 = "/home/isak/test/diff/c2";
-	private String result = "/home/isak/test/diff/result";
 	
-	private ArrayList<String> c1resultplus = new ArrayList<String>();
-	private ArrayList<String> c2resultplus = new ArrayList<String>();
-	private ArrayList<String> c1resultspace = new ArrayList<String>();
-	private ArrayList<String> c2resultspace = new ArrayList<String>();
-	
-	
-	public FindParametersInMerge() {
-		gatherData();
-	}
-	
-	private void gatherData() {
+	public static ArrayList<String> gatherData(String c1, String c2, String result) {
+		ArrayList<String> c1resultplus;
+		ArrayList<String> c2resultplus;
+		ArrayList<String> c1resultspace;
+		
 		c1resultplus = getLines(c1, result, s -> s.startsWith("+"), 1);
 		c2resultplus = getLines(c2, result, s -> s.startsWith("+"), 1);
 		
 		c1resultspace = getLines(c1, result, s -> s.startsWith(" "), 0);
-		c2resultspace = getLines(c2, result, s -> s.startsWith(" "), 0);
+		final ArrayList<String> c2resultspace = getLines(c2, result, s -> s.startsWith(" "), 0);
 		
 		c1resultplus.removeIf(line -> c2resultspace.contains(line));
-		c2resultplus.removeIf(line -> c1resultspace.contains(line));
-		
-		System.out.println("c1rp:");
-		printList(c1resultplus);
-		System.out.println("c2rp:");
-		printList(c2resultplus);
+		//c2resultplus.removeIf(line -> c1resultspace.contains(line));
+		return c1resultplus;
 	}
 	
-	private void printList(ArrayList<String> list) {
-		for (String line : list) {
-			System.out.println(line);
-		}
-	}
-	
-	private ArrayList<String> getLines(String c, String result, java.util.function.Predicate<String> tester, int startIndex) {
+	public static ArrayList<String> getLines(String c, String result, java.util.function.Predicate<String> tester, int startIndex) {
 		ArrayList<String> plus = new ArrayList<String>();
 		try {
 			BufferedReader br = Utils.readCommandOutput("diff -c " + c + " " + result);
