4d239ef424abd8ec1dd336247652ac754e540e3a
dollarmannen <dollarmannen@gmail.com>
2016-04-05 09:10:00 +0200

    Lille borstenthorsten 4

--- FindGoodCommits/src/utils/InformationGatherer.java
+++ FindGoodCommits/src/utils/InformationGatherer.java
@@ -1,5 +1,22 @@
 package utils;
 
 public class InformationGatherer {
+	
+	
+	public String getCommitMessage() {
+		return "";
+	}
+	
+	public String getCommitSHA() {
+		return "";
+	}
+	
+	public boolean isPullRequest() {
+		return false;
+	}
+	
+	public String getFileName() {
+		return "";
+	}
 
 }
