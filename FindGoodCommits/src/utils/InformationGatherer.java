a66911b5006872939bfeb4155ce72cc18438f514
dollarmannen <dollarmannen@gmail.com>
2016-04-05 09:23:42 +0200

    Lillebörst

--- FindGoodCommits/src/utils/InformationGatherer.java
+++ FindGoodCommits/src/utils/InformationGatherer.java
@@ -1,7 +1,15 @@
 package utils;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class InformationGatherer {
 	
+	private String cftRepo;
+	
+	public InformationGatherer(String cftRepo) {
+		this.cftRepo = cftRepo;
+	}
 	
 	public String getCommitMessage() {
 		return "";
@@ -12,11 +20,26 @@ public class InformationGatherer {
 	}
 	
 	public boolean isPullRequest() {
+		if (getCommitMessage().contains("Merge pull request #"))
+			return true;
+		
 		return false;
 	}
 	
-	public String getFileName() {
-		return "";
+	public ArrayList<String> getConflictFileNames(String commit) {
+		ArrayList<String> javaFiles = new ArrayList<String>();
+		File repo = new File(cftRepo + "/" + commit);
+		for(File javaFile : getFiles(f -> f.isDirectory(), repo))
+			javaFiles.add(javaFile.getName());
+		return null;
 	}
-
+	
+	private ArrayList<File> getFiles(java.util.function.Predicate<File> tester, File folder) {
+		ArrayList<File> listOfFiles = new ArrayList<File>();
+		for(File file : folder.listFiles()) {
+			if(tester.test(file))
+				listOfFiles.add(file);
+		}
+		return listOfFiles;
+	}
 }
