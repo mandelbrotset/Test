25900c232f673df029b0550b7389eb91e4989536
Isak Eriksson <isak.eriksson@mail.com>
2016-04-05 11:21:11 +0200

    implemented

--- FindGoodCommits/src/utils/InformationGatherer.java
+++ FindGoodCommits/src/utils/InformationGatherer.java
@@ -1,14 +1,36 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class InformationGatherer {
 	
 	
-	public String getCommitMessage() {
-		return "";
+	public String getCommitMessage(String repo, String commitSHA) {
+		Process msgProcess;
+		try {
+			msgProcess = Runtime.getRuntime().exec("bash " + "scripts/" + "getCommitMessage " + repo + " " + commitSHA);
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(msgProcess.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = br2.readLine()) != null) {
+				if (sb.length() != 0)
+					sb.append("\n");
+				sb.append(line);
+			}
+			return sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "puttepung";
 	}
 	
-	public String getCommitSHA() {
-		return "";
+	public String getCommitSHA(String commitPath) {
+		String[] folders = commitPath.split("/");
+		String commitSHA = folders[folders.length - 1];
+		return commitSHA;
 	}
 	
 	public boolean isPullRequest() {
