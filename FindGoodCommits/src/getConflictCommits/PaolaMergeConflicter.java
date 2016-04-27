3e9b49adc577280cccbe82b229d896edad64f3a4
dollarmannen <dollarmannen@gmail.com>
2016-04-28 00:27:34 +0200

    More stable Paola

--- FindGoodCommits/src/getConflictCommits/PaolaMergeConflicter.java
+++ FindGoodCommits/src/getConflictCommits/PaolaMergeConflicter.java
@@ -23,9 +23,7 @@ public class PaolaMergeConflicter {
 	private Machine machine;
 
 	public PaolaMergeConflicter() {
-		conflictShas = new ArrayList<String>();
 		machine = Machine.getInstance();
-		mergeCommits = new ArrayList<MergeCommit>();
 	}
 
 	/**
@@ -33,51 +31,70 @@ public class PaolaMergeConflicter {
 	 * 
 	 * @return true if there were conflicting files, false otherwise
 	 */
-	public ArrayList<MergeCommit> doMerge(String mergeCommit, String repoPath) {
+	public ArrayList<MergeCommit> doMerge(String repoPath) {
 		REPO = repoPath;
-		try {
-			String ancestrals = Utils.readScriptOutput("getAncestralCommits " + REPO + " " + mergeCommit, true)
-					.readLine();
-			String leftCommit = ancestrals.substring(0, 7);
-			String rightCommit = ancestrals.substring(8, 15);
-			String branchName = "tempbranch";
+		ArrayList<MergeCommit> mergeCommits = new ArrayList<MergeCommit>();
+		ArrayList<String> conflictShas = new ArrayList<String>();
+		ArrayList<String> allCommits = getAllCommits(repoPath, false);
+		for (String mergeCommit : getAllCommits(repoPath, true)) {
+			try {
+				String ancestrals = Utils.readScriptOutput("getAncestralCommits " + REPO + " " + mergeCommit, true)
+						.readLine();
+				String leftCommit = ancestrals.substring(0, 7);
+				String rightCommit = ancestrals.substring(8, 15);
+				
+				for(String commit : allCommits) {
+					if(commit.startsWith(leftCommit))
+						leftCommit = commit;
+					else if(commit.startsWith(rightCommit))
+						rightCommit = commit;
+				}
+				
+				String branchName = "tempbranch";
 
-			BufferedReader br = Utils.readScriptOutput("mergeHistorical " + REPO + " " + leftCommit + " " + rightCommit + " " + branchName, true);
+				BufferedReader br = Utils.readScriptOutput(
+						"mergeHistorical " + REPO + " " + leftCommit + " " + rightCommit + " " + branchName, true);
 
-			String line;
-			final String conflictingLinePattern = machine.getConflictMessage();
-			while ((line = br.readLine()) != null) {
-				if (line.startsWith(conflictingLinePattern) && line.endsWith(".java")) {
-					if (!conflictShas.contains(mergeCommit)) {
-						System.out.println("CONFLICT FOUND");
-						conflictShas.add(mergeCommit);
-						MergeCommit mc = new MergeCommit();
-						mc.setSha(mergeCommit);
-						mc.setParent1(leftCommit);
-						mc.setParent2(rightCommit);
-						mc.setParentsAreDifferent(true);
-						mc.setDate(new Date((long) 1000));
-						System.out.println("ADDING MC: " + mc.getSha());
-						mergeCommits.add(mc);
-						break;
+				String line;
+				final String conflictingLinePattern = machine.getConflictMessage();
+				while ((line = br.readLine()) != null) {
+					if (line.startsWith(conflictingLinePattern) && line.endsWith(".java")) {
+						if (!conflictShas.contains(mergeCommit)) {
+							System.out.println("CONFLICT FOUND");
+							conflictShas.add(mergeCommit);
+							MergeCommit mc = new MergeCommit();
+							mc.setSha(mergeCommit);
+							mc.setParent1(leftCommit);
+							mc.setParent2(rightCommit);
+							mc.setParentsAreDifferent(true);
+							mc.setDate(new Date((long) 1000));
+							System.out.println("ADDING MC: " + mc.getSha());
+							mergeCommits.add(mc);
+							break;
+						}
 					}
 				}
-			}
-			Utils.readScriptOutput("abortMerge " + REPO, false);
-			Utils.readScriptOutput("deleteBranch " + REPO + " " + branchName, false);
+				Utils.readScriptOutput("abortMerge " + REPO, false);
+				Utils.readScriptOutput("deleteBranch " + REPO + " " + branchName, false);
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		return mergeCommits;
 	}
 
-	private ArrayList<String> getTotalMergeCommits(String repoPath) {
+	
+	private ArrayList<String> getAllCommits(String repoPath, boolean onlyMergeCommits) {
 		ArrayList<String> shas = new ArrayList<String>();
 		try {
-			BufferedReader br = Utils.readScriptOutput("getCommits " + repoPath, true);
+			BufferedReader br;
+			if(onlyMergeCommits)
+				br = Utils.readScriptOutput("getCommits " + repoPath, true);
+			else
+				br = Utils.readScriptOutput("getAllCommits " + repoPath, true);
 
 			String commitSHA;
 			while ((commitSHA = br.readLine()) != null) {
